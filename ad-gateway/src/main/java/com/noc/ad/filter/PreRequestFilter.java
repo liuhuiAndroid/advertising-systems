0ee59eb1909823ef78256f41254366a929d9b7a9
liu hui <im.lh@hotmail.com>
2019-03-16 21:16:51 +0800

    网关工程创建

---
+++ ad-gateway/src/main/java/com/noc/ad/filter/PreRequestFilter.java
@@ -0,0 +1,54 @@
+package com.noc.ad.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class PreRequestFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    /**
+     * 定义filter执行的顺序，数字越小顺序越高
+     *
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 是否需要执行此过滤器
+     *
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        // 编写是否需要执行此过滤器逻辑
+        return true;
+    }
+
+    /**
+     * filter执行的具体操作
+     *
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ctx.set("startTime", System.currentTimeMillis());
+
+        return null;
+    }
+}
